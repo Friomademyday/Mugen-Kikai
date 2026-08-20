9e82e1b070bc4790466e829ae1208616de42b5fa
Friomademyday <118212222+Friomademyday@users.noreply.github.com>
2026-08-20 12:40:26 +0100

    Add Command and CommandContext interfaces

--- src/types/command.ts
+++ src/types/command.ts
@@ -1,1 +1,20 @@
+import { WASocket, proto } from '@whiskeysockets/baileys';
 
+export interface CommandContext {
+  sock: WASocket;
+  msg: proto.IWebMessageInfo;
+  from: string;
+  sender: string;
+  args: string[];
+  command: string;
+  text: string;
+  isGroup: boolean;
+}
+
+export interface Command {
+  name: string;
+  category: string;
+  aliases?: string[];
+  description: string;
+  execute: (ctx: CommandContext) => Promise<void>;
+}
