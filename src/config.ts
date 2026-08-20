3ded6366b31feb971043d42592726c73da5dd324
Friomademyday <118212222+Friomademyday@users.noreply.github.com>
2026-08-20 12:32:11 +0100

    Add dotenv configuration for environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
 
+export const CONFIG = {
+
+  prefix: process.env.BOT_PREFIX || 'π',
+  mongoUri: process.env.MONGO_URI || '',
+  ownerNumber: process.env.OWNER_NUMBER || '',
+  sessionData: process.env.SESSION_DATA || ''
+};
